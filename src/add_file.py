330dd3af846de4e66af12e1067386f8323d019f0
auroredelafouchardiere <aurore.dlf@orange.fr>
2023-03-02 17:10:01 +0100

    created files for fu,ctions

---
+++ src/add_file.py
@@ -0,0 +1,10 @@
+# Define a function to be able to select a file from the computer and use it
+
+def add_file():
+    filename = filedialog.askopenfilename(initialdir="/", title="Select File",
+                                          filetypes=[("Excel files", "*.xlsx"), ("All files", "*.*")])
+    if filename:
+        global data
+        data = pd.read_excel(filename)
+        # Update the label to display the name of the selected file
+        file_label.config(text=filename.split("/")[-1])
