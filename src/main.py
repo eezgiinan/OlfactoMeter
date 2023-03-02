a67240ffd62d473f1ea4a97661356c313e3afe89
auroredelafouchardiere <aurore.dlf@orange.fr>
2023-03-02 12:05:41 +0100

    adding the display data function

--- src/main.py
+++ src/main.py
@@ -109,6 +109,25 @@ def close():
 window = Tk()
 window.geometry("600x450")
 
+def DisplayData():
+    # Create a new window
+    data_window = Toplevel(window)
+    data_window.title("Data")
+    data_window.geometry("600x450")
+
+    # Create a text box to display the data
+    data_text = Text(data_window)
+    data_text.pack(fill=BOTH, expand=YES)
+
+    # Check if data exists
+    if 'data' in globals():
+        # Convert data to a string and insert it into the text box
+        data_str = str(data)
+        data_text.insert(END, data_str)
+    else:
+        # If no data exists, display a message in the text box
+        data_text.insert(END, "No data available")
+
 # Create buttons for each mode of operation and associate them with the corresponding function
 buttons = [Button(window, text=x.name.replace('_', ' '), command=lambda: on_button_click(x)) for x in Modes]
 buttons.extend([Button(window, text=str(x.value), command=lambda: on_button_click(x)) for x in Controls])
