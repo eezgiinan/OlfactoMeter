7be851fdeb891f74652f09dc501c7f5cb55055f2
auroredelafouchardiere <aurore.dlf@orange.fr>
2023-03-07 17:28:44 +0100

    deleted unuseful files

--- src/start_Experiment.py
+++
@@ -1,8 +0,0 @@
-#function to take the data from the excel file and start the experiment
-
-def start_Experiment():
-    if 'data' not in globals():
-        print("Please select a data file first")
-        return
-    for index, row in data.iterrows():
-        set_mode(Modes[row['mode'].title()], row['duration'])
