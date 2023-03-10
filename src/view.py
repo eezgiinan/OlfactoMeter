1ef0fd99d3729f49d49e484d8a0ef07b131d1ad0
eezgiinan <ezginan_inan@hotmail.com>
2023-03-10 15:45:17 +0100

    arranged comments

--- src/view.py
+++ src/view.py
@@ -22,48 +22,31 @@ class View(ttk.Frame):
         # creates a text box and saves the value of the box in duration_var
         self.duration_var = tk.StringVar()
         self.duration_box = ttk.Entry(self, textvariable=self.duration_var, width=30)
-        self.duration_box.grid(row=1, column=1, sticky=tk.NSEW)
-
-        """
-        # creates a button with a label Print on it. When clicked invokes the method print_button_clicked
-        self.print_button = ttk.Button(self, text='Print', command=self.print_button_clicked)
-        self.print_button.grid(row=1, column=2, padx=10)
-        """
+        self.duration_box.grid(row=1, column=1)
 
         # creates label for mode box
         self.mode = ttk.Label(self, text='Select the mode:')
-        self.mode.grid(row=2, column=0)
+        self.mode.grid(row=2, column=0, padx=10)
 
-        # creates a text box and saves the number of odor in odor_num_var
+        """
+        # creates a text box and saves the mode in mode_var.
+        Keeping it here in case we cannot solve the drop-down box issue
         self.mode_var = tk.StringVar()
         self.mode_box = ttk.Entry(self, textvariable=self.mode_var, width=30)
         self.mode_box.grid(row=2, column=1, sticky=tk.NSEW)
-
-        # creates a button for odor
-        self.odor_button = ttk.Button(self, text='Run manual', command=self.run_mode_click)
-        self.odor_button.grid(row=2, column=2, padx=10)
-        # set the disabled flag
-        # self.odor_button.state(['disabled'])
-
+        
         # Run Experiment button
         self.run_exp_button = ttk.Button(self, text='Run file', command=self.run_experiment)
         self.run_exp_button.grid(row=6, column=1, padx=10)
-
-        # creates a button for purging
-        self.purging_button = ttk.Button(self, text='Purging', command=self.purging_button_clicked)
-        self.purging_button.grid(row=3, column=2, padx=10)
-
-        # creates a button for resting
-        self.resting_button = ttk.Button(self, text='Resting', command=self.resting_button_clicked)
-        self.resting_button.grid(row=4, column=2, padx=10)
-        """
+        
         # creates a button for stop
         self.stop_button = tk.Button(self, text='Purge and Stop', fg='red', command=self.purge_stop_clicked)
         self.stop_button.grid(row=7, column=2, padx=10)
-        """
+        
         # creates a button for adding an Excel file
         self.file_button = tk.Button(self, text='Add file', fg='green', command=self.add_file_clicked)
         self.file_button.grid(row=7, column=1, padx=10)
+        """
 
         # adds the menu
         self.bar = self.menubar()
@@ -72,39 +55,19 @@ class View(ttk.Frame):
         # drop down menu for mode selection
         self.drop_var = tk.StringVar()
         self.drop = ttk.Combobox(state="readonly", textvariable=self.drop_var, values=[mode.name for mode in Modes])
-        self.drop.grid(row=2, column=1, padx=10)
+        self.drop.grid(row=2, column=6, padx=10)
 
         # drop down button
         self.drop_button = ttk.Button(self, text='Run drop down', command=self.drop_down_click)
-        self.drop_button.grid(row=2, column=2, padx=10)
+        self.drop_button.grid(row=8, column=1, padx=10)
 
         # message
         self.message_label = ttk.Label(self, text='', foreground='red')
         self.message_label.grid(row=5, column=1, sticky=tk.W)
 
-        # set duration button
-        self.set_duration_button = ttk.Button(self, text='Set Duration', command=self.set_duration)
-        self.set_duration_button.grid(row=5, column=2, padx=10)
-
-        # start countdown button
-        self.start_countdown_button = ttk.Button(self, text='Start Countdown', command=self.start_countdown)
-        self.start_countdown_button.grid(row=6, column=2, padx=10)
-
-        # countdown label
-        self.countdown_label = ttk.Label(
-            self,
-            text=self.time_string(),
-            font=('Digital-7', 40),
-            background='black',
-            foreground='red')
-
-        self.countdown_label.grid(row=4, column=1, padx=10)
-        # schedule an update every 1 second
-        self.countdown_label.after(1000, self.countdown_update)
-
         # Creates colored circles
         self.canvas = tk.Canvas(self, width=100, height=250)
-        self.canvas.grid(row=12, column=1, padx=10)
+        self.canvas.grid(row=17, column=2, padx=10)
 
         # Draw 4 circles
         self.circle1 = self.canvas.create_oval(25, 25, 65, 65, fill='red')
@@ -118,11 +81,11 @@ class View(ttk.Frame):
 
         # set duration button
         self.set_duration_button = ttk.Button(self, text='Set Duration', command=self.set_duration)
-        self.set_duration_button.grid(row=5, column=2, padx=10)
+        self.set_duration_button.grid(row=14, column=2, padx=10)
 
         # start countdown button
         self.start_countdown_button = ttk.Button(self, text='Start Countdown', command=self.start_countdown)
-        self.start_countdown_button.grid(row=6, column=2, padx=10)
+        self.start_countdown_button.grid(row=15, column=2, padx=10)
 
         # countdown label
         self.countdown_label = ttk.Label(
@@ -132,35 +95,14 @@ class View(ttk.Frame):
             background='black',
             foreground='red')
 
-        self.countdown_label.grid(row=4, column=1, padx=10)
+        self.countdown_label.grid(row=15, column=1, padx=10)
         # schedule an update every 1 second
         self.countdown_label.after(1000, self.countdown_update)
 
-    def odor_button_clicked(self):
-        print('Activating odor', self.mode_var.get())
-
     # reads mode and duration values in the text box and activates a thread that executes them
-    def run_mode_click(self):
-        thread = threading.Thread(target=self.controller.activate_mode, args=(self.mode_var.get(), self.duration_var.get(), ))
-        thread.start()
-
     def drop_down_click(self):
         print('Activating drop down', self.drop_var.get())
         thread = threading.Thread(target=self.controller.activate_mode, args=(self.drop_var.get(), self.duration_var.get(), ))
-        thread.start()
-
-    def purging_button_clicked(self):
-        print('Activating purging')
-        # Create a new thread (executing unit that can be run in parallel). This in required as the python
-        # code can only execute 1 part of the code at a time. Either the UI, or the long-running method we call
-        thread = threading.Thread(target=self.controller.activate_mode, args=("Purging", self.duration_var.get(), ))
-        thread.start()
-
-    def resting_button_clicked(self):
-        print('Activating resting')
-        # Create a new thread (executing unit that can be run in parallel). This in required as the python
-        # code can only execute 1 part of the code at a time. Either the UI, or the long-running method we call
-        thread = threading.Thread(target=self.controller.activate_mode)
         thread.start()
 
     """
@@ -170,7 +112,7 @@ class View(ttk.Frame):
         # code can only execute 1 part of the code at a time. Either the UI, or the long-running method we call
         thread = threading.Thread(target=self.controller.activate_stop)
         thread.start()
-    """
+    
     def add_file_clicked(self):
         print('Add an Excel file')
         # Create a new thread (executing unit that can be run in parallel). This in required as the python
@@ -178,10 +120,7 @@ class View(ttk.Frame):
         thread = threading.Thread(target=self.controller.experiment_from_file)
         thread.start()
     """
-    def print_button_clicked(self):
-        print('In the View. Sending', self.duration_var.get())
-        self.controller.print(self.duration_var.get())
-    """
+
     def set_controller(self, controller):
         """
         Set the controller
