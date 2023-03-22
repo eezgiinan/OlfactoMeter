825f332bc16d57667254ea236fa13d8ff8fd3c7f
Adrien <adrien.dhollande@gmail.com>
2023-03-22 15:24:20 +0100

    implementation of a file saving IRL what's happening

--- n_mvc/n_controller.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from n_model import Olfactometer
-from n_modes import Modes
-
-
-class Controller:
-    def __init__(self, model: Olfactometer, view):
-        self.model: Olfactometer = model
-        self.view = view
